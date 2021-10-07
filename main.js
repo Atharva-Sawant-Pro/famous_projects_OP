cb3fdbbf9b9b6b09e08ef8fd032b3aad6836a95f
Atharva-Sawant-Pro <78305129+Atharva-Sawant-Pro@users.noreply.github.com>
2021-10-07 10:40:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function encrypter(){
+    window.location="https://community.whitehatjr.com/project/721c8e0c-270e-4c36-8e46-a6d015502551?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function decrypter(){
+    window.location="https://community.whitehatjr.com/project/181241be-e10f-4931-bb34-092ee6b48085?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function secret(){
+    window.location="https://community.whitehatjr.com/project/694b5ceb-c7f8-483a-bdac-69550844f5d1?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function weight(){
+    window.location="https://community.whitehatjr.com/project/77568258-0502-482e-a011-35dc2b618279?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function moustache(){
+    window.location="https://community.whitehatjr.com/project/2b658da7-ecf0-4abc-afa4-238ee4c7e22b?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function dj(){
+    window.location="https://community.whitehatjr.com/project/e739cc8f-ef3e-474d-882d-4d6d385a269e?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function antarikshyaan(){
+    window.location="https://community.whitehatjr.com/project/112122a7-77b0-48ea-8f53-c603c144e612?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function juno(){
+    window.location="https://community.whitehatjr.com/project/9d584108-fb56-4c38-9052-7488eb93fc0e?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function colour(){
+    window.location="https://community.whitehatjr.com/project/4d139559-d3e4-4fec-b40c-1a981f46ac02?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function webrowser(){
+    window.location="https://community.whitehatjr.com/project/059a146c-3a45-413c-ae66-c9ad0958339d?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
+function tt(){
+    window.location="https://community.whitehatjr.com/project/5751a32a-730f-4e3a-ba03-0d8cfb11abdc?utm_source=student_community&utm_medium=copy_link&utm_campaign=sharing_flow"
+}
